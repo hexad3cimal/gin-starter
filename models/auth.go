32835cf96478d3decf6946ef31f96cdd38964580
Jovin <jovin.war@gmail.com>
2020-08-04 23:28:27 +0530

    add auth base

---
+++ models/auth.go
@@ -0,0 +1,163 @@
+package models
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	uuid "github.com/twinj/uuid"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type TokenDetails struct {
+	AccessToken  string
+	RefreshToken string
+	AccessUUID   string
+	RefreshUUID  string
+	AtExpires    int64
+	RtExpires    int64
+}
+
+type AccessDetails struct {
+	AccessUUID string
+	Email     string
+}
+
+type Token struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type AuthModel struct{}
+
+func (m AuthModel) CreateToken(email int64) (*TokenDetails, error) {
+
+	td := &TokenDetails{}
+	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.AccessUUID = uuid.NewV4().String()
+
+	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+	td.RefreshUUID = uuid.NewV4().String()
+
+	var err error
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["access_uuid"] = td.AccessUUID
+	atClaims["user_id"] = email
+	atClaims["exp"] = td.AtExpires
+
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	td.AccessToken, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUUID
+	rtClaims["user_id"] = email
+	rtClaims["exp"] = td.RtExpires
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+	return td, nil
+}
+
+func (m AuthModel) CreateAuth(email string, td *TokenDetails) error {
+	at := time.Unix(td.AtExpires, 0) //converting Unix to UTC(to Time object)
+	rt := time.Unix(td.RtExpires, 0)
+	now := time.Now()
+
+	errAccess := db.GetRedis().Set(td.AccessUUID, email, at.Sub(now)).Err()
+	if errAccess != nil {
+		return errAccess
+	}
+	errRefresh := db.GetRedis().Set(td.RefreshUUID, email, rt.Sub(now)).Err()
+	if errRefresh != nil {
+		return errRefresh
+	}
+	return nil
+}
+
+//ExtractToken ...
+func (m AuthModel) ExtractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	//normally Authorization the_token_xxx
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
+}
+
+//VerifyToken ...
+func (m AuthModel) VerifyToken(r *http.Request) (*jwt.Token, error) {
+	tokenString := m.ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+//TokenValid ...
+func (m AuthModel) TokenValid(r *http.Request) error {
+	token, err := m.VerifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+	return nil
+}
+
+//ExtractTokenMetadata ...
+func (m AuthModel) ExtractTokenMetadata(r *http.Request) (*AccessDetails, error) {
+	token, err := m.VerifyToken(r)
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		accessUUID, ok := claims["access_uuid"].(string)
+		if !ok {
+			return nil, err
+		}
+		email :=  claims["user_id"].(string)
+		if err != nil {
+			return nil, err
+		}
+		return &AccessDetails{
+			AccessUUID: accessUUID,
+			Email:email,
+		}, nil
+	}
+	return nil, err
+}
+
+//FetchAuth ...
+func (m AuthModel) FetchAuth(authD *AccessDetails) (int64, error) {
+	userid, err := db.GetRedis().Get(authD.AccessUUID).Result()
+	if err != nil {
+		return 0, err
+	}
+	userID, _ := strconv.ParseInt(userid, 10, 64)
+	return userID, nil
+}
+
+//DeleteAuth ...
+func (m AuthModel) DeleteAuth(givenUUID string) (int64, error) {
+	deleted, err := db.GetRedis().Del(givenUUID).Result()
+	if err != nil {
+		return 0, err
+	}
+	return deleted, nil
+}
